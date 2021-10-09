ea71bc5a64b465616e06e94ec516a74302fa5bf6
RafaelLiWu <infocriffitaperuna@gmail.com>
2021-10-09 17:13:59 -0300

    .

--- js/jss.js
+++ js/jss.js
@@ -1,3 +1,25 @@
+let s=e=>document.querySelector(e);
+
+usersJson.map((e)=>{
+  let carousel_item = document.querySelector('.carousel-item').cloneNode(true);
+  let s_item=e=>carousel_item.querySelector(e)
+  document.querySelector('.carousel-inner').append(carousel_item);
+  let carousel_item_all = document.querySelectorAll('.carousel-item');
+  if( carousel_item_all.length == 7 ) {
+  carousel_item_all[0].style.display = 'none';
+  carousel_item_all[1].classList.add('active');
+  }
+  s_item('.foto-img img').src = `assets/${e.img}`;
+  s_item('.guy-name h6').innerHTML = e.nome;
+  s_item('.guy-name small').innerHTML = e.turma;
+  s_item('.content p').innerHTML = e.texto;
+  s_item('#github').href = e.github;
+  s_item('#instagram').href = e.instagram;
+  s_item('#facebook').href = e.facebook;
+});
+
+
+
 
 $(function () {
   $('[data-toggle="popover"]').popover()
---
+++ js/usersJson.js
@@ -0,0 +1,74 @@
+let usersJson = [
+
+	{
+	id:1,
+	nome: 'Carlos Daniel',
+	turma: 'Informática 2A',
+	img: 'icon.jpg',
+	texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur semper tortor eu consectetur. Cras vel maximus orci, at fermentum sapien. Vivamus et blandit velit, sed viverra elit. Proin eget laoreet ex. Curabitur vel ligula lorem. Nam tortor ante, fermentum sed ultrices efficitur, lacinia in ante. Curabitur bibendum quam dui, eu graa metus volutpat quis.',
+	instagram: '#',
+	github: '#',
+	facebook: '#'
+	},
+
+
+	{
+	id:2,
+	nome: 'Carlos Sérgio',
+	turma: 'Informática 2A',
+	img: 'img1.png',
+	texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur semper tortor eu consectetur. Cras vel maximus orci, at fermentum sapien. Vivamus et blandit velit, sed viverra elit. Proin eget laoreet ex. Curabitur vel ligula lorem. Nam tortor ante, fermentum sed ultrices efficitur, lacinia in ante. Curabitur bibendum quam dui, eu graa metus volutpat quis.',
+	instagram: '#',
+	github: '#',
+	facebook: '#'
+	},
+
+
+	{
+	id:3,
+	nome: 'Nathan',
+	turma: 'Ex-Aluno',
+	img: 'image13.jpg',
+	texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur semper tortor eu consectetur. Cras vel maximus orci, at fermentum sapien. Vivamus et blandit velit, sed viverra elit. Proin eget laoreet ex. Curabitur vel ligula lorem. Nam tortor ante, fermentum sed ultrices efficitur, lacinia in ante. Curabitur bibendum quam dui, eu graa metus volutpat quis.',
+	instagram: '#',
+	github: '#',
+	facebook: '#'
+	},
+
+
+	{
+	id:4,
+	nome: 'Rafael Li Wu',
+	turma: 'Informática 2B',
+	img: 'rafaelli.jpg',
+	texto: 'Meu nome é Rafael Li Wu e sou estudante do Instituo Federal Fluminense (IFF), cursando Informática, atualmente sou um aluno normal com taxas de 90% de erros em qualquer coisa que eu faço',
+	instagram: '#',
+	github: 'https://github.com/RafaelLiWu',
+	facebook: '#'
+	},
+
+
+	{
+	id:5,
+	nome: 'Rafael Soares',
+	turma: 'Informática 2B',
+	img: 'rafaelsoares.jpg',
+	texto: 'Meu nome é Rafael Soares, tenho 16 anos, e estudo no segundo ano, da turma de Informática, no IFF Campus Itaperuna. Eu gosto bastante de programação, e desde que tive acesso ao meu primeiro computador, tento aprender mais sobre essa área.</p><p class="text-light">Eu tenho um conhecimento Intermediário em GDScript, e Iniciante em Python, já fiz alguns projetinhos interessantes, e guardo eles no meu github, "Ochernobas".</p>',
+	instagram: '#',
+	github: '#',
+	facebook: '#'
+	},
+
+
+	{
+	id:6,
+	nome: 'Ronald Oliveira',
+	turma: 'Informática 2B',
+	img: 'image4.jpg',
+	texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur semper tortor eu consectetur. Cras vel maximus orci, at fermentum sapien. Vivamus et blandit velit, sed viverra elit. Proin eget laoreet ex. Curabitur vel ligula lorem. Nam tortor ante, fermentum sed ultrices efficitur, lacinia in ante. Curabitur bibendum quam dui, eu graa metus volutpat quis.',
+	instagram: '#',
+	github: '#',
+	facebook: '#'
+	},
+	
+];
